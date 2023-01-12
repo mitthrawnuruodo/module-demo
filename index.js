07db2aa17f53c4b6de080f0bd9d04c477f622997
Lasse Hægland <seilenos@gmail.com>
2023-01-12 15:51:45 +0100

    post-demo-time

--- index.js
+++ index.js
@@ -40,4 +40,55 @@ document.getElementById("submit-currency").addEventListener('click', () => {
     currencyConverter(amount, from, to, out);
 });
 
-getAllCurrencies();
+const fromCurrencyList = document.getElementById("convertFromSymbols");
+const toCurrencyList = document.getElementById("convertToSymbols");
+getAllCurrencies(fromCurrencyList, toCurrencyList); 
+//getAllCurrencies();
+
+/** 
+ * Amiibos 
+ */
+import { getAllAmiibos, filterAmiibos } from './utils/amiibo.mjs';
+const outPutElement = document.getElementById("amiibos");
+getAllAmiibos(outPutElement);
+
+let searchBox = document.getElementById("search-amiibos");
+searchBox.addEventListener('input', updateSearch);
+
+let nameCheckBox = document.getElementById("amiibo-name");
+nameCheckBox.addEventListener('input', updateSearch);
+
+let gameSeriesCheckBox = document.getElementById("amiibo-game");
+gameSeriesCheckBox.addEventListener('input', updateSearch);
+
+let amiiboSeriesCheckbox = document.getElementById("amiibo-series")
+amiiboSeriesCheckbox.addEventListener('input', updateSearch);
+
+function updateSearch() {
+    let searchString = searchBox.value;
+    let nameOpt = nameCheckBox.checked;
+    let gameSeriesOpt = gameSeriesCheckBox.checked;
+    let amiiboSeriesOpt = amiiboSeriesCheckbox.checked;
+
+    let options = { nameOpt, gameSeriesOpt, amiiboSeriesOpt };
+    //console.log(`Will search for ${searchString}`);
+    console.log(`Will search for ${searchString}`, options);
+    //filterAmiibos(searchString, outPutElement);
+    filterAmiibos(searchString, outPutElement, options);
+}
+
+document.getElementById("openAll").addEventListener("click", () => {
+    toggleDetails("open");
+});
+document.getElementById("closeAll").addEventListener("click", () => {
+    toggleDetails("close");
+});
+
+function toggleDetails(mode) {
+    let detailsElements = document.querySelectorAll("details");
+    //console.log(detailsElements.length);
+    for (let element of detailsElements) {
+        if (mode==="open") element.setAttribute('open', true);
+        if (mode==="close") element.removeAttribute('open');
+    }
+}
--- utils/currency.mjs
+++ utils/currency.mjs
@@ -1,7 +1,7 @@
 import { round } from './utils.mjs';
 
 export async function currencyConverter(amount, from, to, out) {
-    console.log(`Convert ${from} ${amount} into ${to} and put in ${out}`);
+    //console.log(`Convert ${from} ${amount} into ${to} and put in ${out}`);
 
     const requestURL = `https://api.exchangerate.host/convert?from=${from}&to=${to}`;
 /*
@@ -24,17 +24,27 @@ export async function currencyConverter(amount, from, to, out) {
 
 };
 
-export async function getAllCurrencies() {
+export async function getAllCurrencies(fromList, toList) {
 
     var requestURL = 'https://api.exchangerate.host/symbols';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    
-    request.onload = function() {
-      var response = request.response;
-      console.log(response.symbols);
-    }
+    fetch(requestURL)
+    .then((response) => response.json())
+    .then((data) => {
+        //console.log(data.symbols);
+        let currencyList = "";
+        for (let symbol in data.symbols) {
+            // Using Datalist for options: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist
+            // Template: <option value="NOK">Norwegian Krone (NOK)</option>
+            //console.log (data.symbols[symbol].description, data.symbols[symbol].code);
+            let code = data.symbols[symbol].code; // Is this always the same as symbol?
+            let desc = data.symbols[symbol].description;
+            currencyList += `<option value="${code}">${desc} (${code})</option>\n`;
+            //console.log(currencyList);
+        }
+        if (currencyList) {
+            fromList.innerHTML = currencyList;
+            toList.innerHTML = currencyList;
+        }
+    });
 
 };
